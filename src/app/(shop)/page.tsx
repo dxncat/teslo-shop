bb8f06a15d0b1f3ceef888c1d69d356f1805f5d8
dxncat <xidarkmoon@gmail.com>
2025-03-03 16:12:51 -0500

    Agregar componente ProductGrid para mostrar productos y definir la interfaz del producto

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,9 +1,17 @@
-import { Title } from "@/components";
+import { ProductGrid, Title } from "@/components";
+import { initialData } from "@/seed/seed";
+
+const products = initialData.products;
 
 export default function Home() {
   return (
     <>
-      <Title title="Tienda" subtitle="Todos los productos" />
+      <Title
+        title="Tienda"
+        subtitle="Todos los productos"
+      />
+
+      <ProductGrid products={products} />
     </>
   );
 }
