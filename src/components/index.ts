90bbaaebf3c6f6f83649ea0689b4f778da2b59e4
dxncat <xidarkmoon@gmail.com>
2025-03-10 18:24:42 -0500

    Agregar página de carrito vacío con enlace de regreso y footer

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,7 @@ export * from "./ui/not-found/PageNotFound";
 export * from "./ui/sidebar/SideBar";
 export * from "./ui/title/Title";
 export * from "./ui/top-menu/TopMenu";
+export * from "./ui/footer/Footer";
 
 export * from "./products/product-grid/ProductGrid";
 export * from "./products/product-grid/ProductGridItem";
