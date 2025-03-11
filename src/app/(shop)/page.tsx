2df3b443765dbd3d370a8cc8b4016aa50d6ef86a
dxncat <xidarkmoon@gmail.com>
2025-03-11 14:49:36 -0500

    Implementar paginación de productos y eliminar página de categoría por ID

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,9 +1,23 @@
-import { ProductGrid, Title } from "@/components";
-import { initialData } from "@/seed/seed";
+import { getPaginatedProductsWithImages } from "@/actions";
+import { Pagination, ProductGrid, Title } from "@/components";
+import { redirect } from "next/navigation";
 
-const products = initialData.products;
+interface Props {
+  searchParams: {
+    page?: string
+  }
+}
 
-export default function Home() {
+export default async function ({ searchParams }: Props) {
+
+  const page = searchParams.page ? parseInt(searchParams.page) : 1
+
+  const { products, totalPages, currentPage } = await getPaginatedProductsWithImages({ page })
+
+  if (products.length === 0) {
+    redirect('/')
+  }
+
   return (
     <>
       <Title
@@ -12,6 +26,8 @@ export default function Home() {
       />
 
       <ProductGrid products={products} />
+
+      <Pagination totalPages={totalPages} />
     </>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,7 @@ export * from "./ui/sidebar/SideBar";
 export * from "./ui/title/Title";
 export * from "./ui/top-menu/TopMenu";
 export * from "./ui/footer/Footer";
+export * from "./ui/pagination/Pagination";
 
 export * from "./products/product-grid/ProductGrid";
 export * from "./products/product-grid/ProductGridItem";
