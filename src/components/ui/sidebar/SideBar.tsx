66cd138336b35fcced5b85af96fdcdc778c67167
dxncat <xidarkmoon@gmail.com>
2025-03-17 16:52:07 -0500

    Agregar modelo User con campos de autenticación y rol, y actualizar la barra lateral para redirigir al inicio de sesión

--- src/components/ui/sidebar/SideBar.tsx
+++ src/components/ui/sidebar/SideBar.tsx
@@ -69,7 +69,7 @@ export const SideBar = () => {
                 </Link>
 
                 <Link
-                    href='/'
+                    href='/auth/login'
                     className='flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all'>
                     <IoLogInOutline size={30} />
                     <span className='ml-3 text-xl'>Iniciar sesión</span>
