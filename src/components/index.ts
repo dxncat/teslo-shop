2a81c715d25feeca362936617232f4e3c8118c63
dxncat <xidarkmoon@gmail.com>
2025-03-17 16:42:19 -0500

    Agregar autenticación con NextAuth y crear formulario de inicio de sesión

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,16 +1,16 @@
+export * from "./ui/footer/Footer";
 export * from "./ui/not-found/PageNotFound";
+export * from "./ui/pagination/Pagination";
 export * from "./ui/sidebar/SideBar";
 export * from "./ui/title/Title";
 export * from "./ui/top-menu/TopMenu";
-export * from "./ui/footer/Footer";
-export * from "./ui/pagination/Pagination";
 
 export * from "./products/product-grid/ProductGrid";
 export * from "./products/product-grid/ProductGridItem";
 
-export * from './product/size-selector/SizeSelector';
-export * from './product/quantity-selector/QuantitySelector';
-export * from './product/slideshow/Slideshow';
 export * from './product/product-image/ProductImage';
+export * from './product/quantity-selector/QuantitySelector';
+export * from './product/size-selector/SizeSelector';
 export * from './product/slideshow/MobileSlideshow';
+export * from './product/slideshow/Slideshow';
 export * from './product/stock-label/StockLabel';
