a07380998c1abc103a1609f90a7ed52a8989bc88
dxncat <xidarkmoon@gmail.com>
2025-03-17 17:29:32 -0500

    Agregar manejo de estado de carga y mensaje de error en el formulario de inicio de sesión

--- src/app/auth/login/ui/LoginForm.tsx
+++ src/app/auth/login/ui/LoginForm.tsx
@@ -1,8 +1,11 @@
 'use client'
 
 import { authenticate } from "@/actions"
+import clsx from "clsx"
 import Link from "next/link"
 import { useActionState } from "react"
+import { useFormStatus } from "react-dom"
+import { IoInformationOutline } from "react-icons/io5"
 
 export const LoginForm = () => {
 
@@ -26,11 +29,14 @@ export const LoginForm = () => {
                 name="password"
             />
 
-            <button
-                type="submit"
-                className="btn-primary">
-                Ingresar
-            </button>
+            {state === "CredentialsSignin" && (
+                <div className="flex flex-row mb-2">
+                    <IoInformationOutline className="h-5 w-5 text-red-500" />
+                    <p className="text-sm text-red-500">Credenciales Invalidas</p>
+                </div>
+            )}
+
+            <LoginButton />
 
 
             {/* divisor l ine */}
@@ -47,5 +53,22 @@ export const LoginForm = () => {
             </Link>
 
         </form>
+    )
+}
+
+function LoginButton() {
+    const { pending } = useFormStatus();
+
+    return (
+        <button
+            type="submit"
+            className={clsx({
+                "btn-primary": !pending,
+                "btn-disabled": pending
+            })}
+            disabled={pending}
+        >
+            Ingresar
+        </button>
     )
 }
