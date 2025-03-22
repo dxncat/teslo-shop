fbf78f70cf357bf0a7a9189173fb85d05534a512
dxncat <xidarkmoon@gmail.com>
2025-03-22 11:46:13 -0500

    Agregar el componente Provider en el layout y exportar manejadores de autenticación

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Provider } from "@/components";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -24,7 +25,9 @@ export default function RootLayout({
       <body
         className={`${inter.className} antialiased`}
       >
-        {children}
+        <Provider>
+          {children}
+        </Provider>
       </body>
     </html>
   );
